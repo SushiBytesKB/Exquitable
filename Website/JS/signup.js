d484fbfaa6e60b586465fae7b432450087e5138a
BettinaDev <marksteinerbettina@ymail.com>
2025-11-24 17:22:50 +0900

    updated signup

---
+++ Website/JS/signup.js
@@ -0,0 +1,56 @@
+const testUsers= [
+
+{
+    email: "admin@exquitable.com",
+    password: "admin"
+},
+{
+    email: "user@exquitable.com",
+    password: "user"
+},
+{
+    email: "guest@exquitable.com",
+    password: "guest"
+}
+];
+
+// sign up needs: email and password
+// login needs: email and password
+
+const signupForm = document.getElementById("signupForm");
+const errorMessage = document.getElementById("errorMessage");
+
+if (signupForm)
+{
+    signupForm.addEventListener("submit", event =>
+    {
+        event.preventDefault();
+
+        const emailInput = document.getElementById("email").value;
+        const passwordInput = document.getElementById("password").value;
+
+        let isAuthenticated = false;
+
+        for (const user of testUsers)
+        {
+            if (user.email == emailInput && user.password == passwordInput)
+            {
+                isAuthenticated = true;
+                break;
+            }
+        }
+
+        if(isAuthenticated)
+        {
+            errorMessage.style.display = "none";
+
+            window.location.href = "index.html";
+
+        }
+        else
+        {
+            errorMessage.style.display = "block";
+            errorMessage.textContent = "Incorrect email or password";
+        }
+    });
+}
